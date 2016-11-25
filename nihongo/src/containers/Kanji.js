1922ffca6f7ee4cbc497093d471818575ef1dcc2
Anh Pham <anhph5@fsoft.com.vn>
2016-11-26 04:36:18 +0700

    button kanji

--- nihongo/src/containers/Kanji.js
+++ nihongo/src/containers/Kanji.js
@@ -6,8 +6,10 @@ import {
 	StyleSheet,
 	TouchableHighlight
 } from 'react-native';
-
+import ButtonComponent from 'react-native-button-component';
 import { Actions } from 'react-native-router-flux';
+
+const backgroundColor = ['#212122', '#414141'];
 
 const styles = StyleSheet.create({
 	container: {
@@ -18,8 +20,11 @@ const styles = StyleSheet.create({
 		height: 40,
 		alignItems: 'center',
 		justifyContent: 'center',
-		marginBottom: 1,
-		backgroundColor: '#9b59b6'
+		backgroundColor: '#FAC7C8',
+		borderColor: '#FF4A29',
+		borderRadius: 10,
+		borderWidth: 1.6,
+		marginTop: 30
 	}
 });
 
@@ -28,23 +33,30 @@ export default class Kanji extends Component {
 	render() {
 		return (
 			<View style={ styles.container }>
-				<TouchableHighlight style={styles.button}
+				<TouchableHighlight
+						style={styles.button}
+						underlayColor='#faebd7'
 						onPress={Actions.flash}	>
 					<Text>
 						Cơ bản
 					</Text>
 				</TouchableHighlight>
-				<TouchableHighlight style={styles.button}>
+				<TouchableHighlight
+						style={styles.button}
+						underlayColor='#faebd7'
+						onPress={Actions.flash}>
 					<Text>
 						Nâng cao
 					</Text>
 				</TouchableHighlight>
-				<TouchableHighlight style={styles.button}>
+				<TouchableHighlight
+						style={styles.button}
+						underlayColor='#faebd7'
+						onPress={Actions.flash}>
 					<Text>
 						Ngẫu nhiên
 					</Text>
 				</TouchableHighlight>
-
 			</View>
 		);
 	}
