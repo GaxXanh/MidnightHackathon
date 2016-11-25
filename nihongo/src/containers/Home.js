d4ce0b6300943fbf84a6bd86cc03ce3b2ebf28b3
ThanhChung <thanhchungbtc@gmail.com>
2016-11-26 04:33:47 +0700

    home page UI

--- nihongo/src/containers/Home.js
+++ nihongo/src/containers/Home.js
@@ -2,24 +2,96 @@ import React, { Component } from 'react';
 import {
 	Text,
 	TouchableHighlight,
-	StyleSheet
+	StyleSheet,
+	View
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux';
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 10
+	},
+	row: {
+		marginTop: 90,
+		flex: 1,
+		flexDirection: 'row'
+	},
+	button: {
+		height: 50,
+		marginLeft: 10,
+		padding: 10,
+		backgroundColor: 'pink',
+		justifyContent: 'center',
 
+		borderRadius: 10,
+		flex: 1
+	},
+	title: {
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+
+		backgroundColor: 'red',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
 
 export default class Home extends Component {
 	render() {
 		return (
-			<TouchableHighlight
-					onPress={ Actions.kanji }>
-				<Text style={{ marginTop: 80 }}>
-					Học Kanji
-				</Text>
-			</TouchableHighlight>
+			<View style={styles.container}>
+				<View style={styles.row}>
+					<View style={styles.title}><Text style={{fontSize: 30, color: 'white'}}>字</Text></View>
+					<TouchableHighlight
+							underlayColor="pink"
+							style={styles.button}
+							onPress={ Actions.kanji }>
+						<Text>
+							Học Kanji
+						</Text>
+					</TouchableHighlight>
+				</View>
+
+				<View style={styles.row}>
+					<View style={styles.title}><Text style={{fontSize: 30, color: 'white'}}>受</Text></View>
+					<TouchableHighlight
+							underlayColor="pink"
+							style={styles.button}
+							onPress={ Actions.kanji }>
+						<Text>
+							Làm bài test
+						</Text>
+					</TouchableHighlight>
+				</View>
+
+				<View style={styles.row}>
+					<View style={styles.title}><Text style={{fontSize: 30, color: 'white'}}>上</Text></View>
+					<TouchableHighlight
+							underlayColor="pink"
+							style={styles.button}
+							onPress={ Actions.kanji }>
+						<Text>
+							Bảng xếp hạng
+						</Text>
+					</TouchableHighlight>
+				</View>
+
+				<View style={styles.row}>
+					<View style={styles.title}><Text style={{fontSize: 30, color: 'white'}}>設</Text></View>
+					<TouchableHighlight
+							underlayColor="pink"
+							style={styles.button}
+							onPress={ Actions.kanji }>
+						<Text>
+							Cài đặt
+						</Text>
+					</TouchableHighlight>
+				</View>
+
+			</View>
 		);
 	}
 }
