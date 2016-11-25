b5b438d44bc6f30b451597bdd569d95f65efb97e
ThanhChung <thanhchungbtc@gmail.com>
2016-11-25 23:40:20 +0700

    Add file base

---
+++ nihongo/src/Main.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+
+/* import router */
+import { Scene, Router, Actions } from 'react-native-router-flux';
+
+/* App's Components */
+import Home from './components/Home';
+import Kanji from './components/Kanji';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="home" component={Home} title="React de Nihongo" initial={true} />
+  </Scene>
+);
+
+export default class Main extends Component {
+
+  render() {
+    return (
+      <Router scenes={scenes} />
+    );
+  }
+}
---
+++ nihongo/src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import {
+	Text,
+	View,
+	TouchableHighlight
+} from 'react-native';
+
+import { Actions } from 'react-native-router-flux';
+
+
+
+export default class Home extends Component {
+	render() {
+		return (
+			<View style={{margin: 128}}>
+				<Text onPress={Actions.kanji}>Home</Text>
+			</View>
+		);
+	}
+}
