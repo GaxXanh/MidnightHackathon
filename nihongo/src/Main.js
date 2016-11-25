c6ec78ddc13d366bad0252e674f74dd2b53c7e3b
ThanhChung <thanhchungbtc@gmail.com>
2016-11-26 03:04:43 +0700

    FlashCard: can flip card

--- nihongo/src/Main.js
+++ nihongo/src/Main.js
@@ -15,30 +15,44 @@ import Home from './containers/Home';
 import Kanji from './containers/Kanji';
 import FlashCard from './containers/FlashCard';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+const KANJI_LIST = [
+    {
+             "kanji":"挙",
+             "description":"a wedding ceremony, a wedding",
+             "romaji":"kyoshiki",
+             "hiragana": "きょしき",
+             "Example" : [{
+                "text": "挙式はいつですか",
+                "translate": "When is your wedding ceremony?"
+             },{
+                "text": "教会で挙式の予定です",
+                "translate": "Our wedding will be held in church."
+             }]
+
+     },
+     {
+             "kanji":"式",
+             "description":"a wedding ceremony, a wedding",
+             "romaji":"kyoshiki",
+             "hiragana": "きょしき",
+             "Example" : [{
+                "text": "挙式はいつですか",
+                "translate": "When is your wedding ceremony?"
+             },{
+                "text": "教会で挙式の予定です",
+                "translate": "Our wedding will be held in church."
+             }]
+
+     }
+
+]
 
 const scenes = Actions.create(
   <Scene key="root">
     <Scene key="home" component={Home} title="React de Nihongo" initial={true} />
     <Scene key="kanji" component={Kanji} />
-    <Scene key="flash" component={FlashCard} title="Flash Card" />
+    <Scene key="flash" component={FlashCard} title="Flash Card" dataSource={KANJI_LIST} />
 
 
   </Scene>
